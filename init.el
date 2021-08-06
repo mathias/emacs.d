5233130db746f0f80c10a0b64113294bafb4807e
Matt Gauger <matt.gauger@gmail.com>
2021-08-06 17:22:54 -0500

    Starting over

--- init.el
+++
@@ -1,8 +0,0 @@
-;; This sets up the load path so that we can override it
-(package-initialize nil)
-
-;;;; Load org in.
-(require 'package)
-(setq package-enable-at-startup nil)
-
-(org-babel-load-file "~/.emacs.d/mathias.org")

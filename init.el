5c4176d66127c02d616deb2f8f1e9cc787e9920a
Matt Gauger <matt.gauger@gmail.com>
2022-07-24 10:46:03 -0500

    Longer timeout, less likely to accidentally see which-key

--- init.el
+++ init.el
@@ -53,7 +53,7 @@
 (use-package which-key
   :init (which-key-mode)
   :diminish which-key-mode
-  :config (setq which-key-idle-delay 0.3))
+  :config (setq which-key-idle-delay 1.0))
 
 (use-package ivy-rich
   :init (ivy-rich-mode 1))
